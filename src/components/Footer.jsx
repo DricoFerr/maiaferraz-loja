d692911f6f02767e3112c6c2151d5fe4a014f23e
DricoFerr <adrianoferrazguimaraes@gmail.com>
2025-05-23 17:02:23 -0300

    Loja MaiaFerraz commit 2

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,13 @@
+// src/components/Footer.jsx
 import React from 'react';
+import '../styles/Footer.css'; // <<<< ADICIONE ESTA LINHA
 
 function Footer() {
-  return (
-    <footer className="main-footer">
-      <p>&copy; {new Date().getFullYear()} Maia Ferraz. Todos os direitos reservados.</p>
-    </footer>
-  );
+  return (
+    <footer className="main-footer">
+      <p>&copy; {new Date().getFullYear()} Maia Ferraz. Todos os direitos reservados.</p>
+    </footer>
+  );
 }
 
 export default Footer;
